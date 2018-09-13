be272b26c869ac46a9d3bf5847e09b8465f5172b
Gustavo Cordeiro <oliv.dg@gmail.com>
2018-09-13 01:04:02 -0300

    bla

--- src/lista-presentes.js
+++ src/lista-presentes.js
@@ -1,9 +1,12 @@
-var gifts = [{
-  "url_imagem": "Lixeira.jpg",
-  "link_compra": "https://www.etna.com.br/etna/p/lixeira-pedal-aco-5litros/047414?skuId=0310852 ",
-  "nome_presente": "Lixeira - R$99,00",
-  "descricao_presente": ""
-}];
+var gifts = [
+  {
+    "url_imagem": "Lixeira.jpg",
+    "link_compra": "https://www.etna.com.br/etna/p/lixeira-pedal-aco-5litros/047414?skuId=0310852 ",
+    "nome_presente": "Lixeira - R$99,00",
+    "descricao_presente": ""
+  },
+  {"url_imagem":"LavaPratos.png","link_compra":"https://www.magazineluiza.com.br/lava-loucas-brastemp-ative-blf08as-8-servicos/p/011704500/ed/lalo/?partner_id=4660&utm_source=google&seller_id=magazineluiza&product_group_id=317393638549&ad_group_id=12213644784&aw_viq=pla&gclid=CjwKCAjw8uLcBRACEiwAaL6MSZvxSXw2Ce5CCVJ2mktUki38kxsTN-8NeUCOHxxyWVua6_vaeOikeBoCnrwQAvD_BwE","nome_presente":"Lava Pratos - R$1.424,91 ","descricao_presente":""},{"url_imagem":"AirFri.jpg","link_compra":"https://www.pontofrio.com.br/Eletroportateis/Fritadeiras/fritadeira-sem-oleo-midea-liva-4-litros-branco-preto-11615781.html?gclid=CjwKCAjw8uLcBRACEiwAaL6MSXQb_ILYp-AS-LYszF9OaAIJf8jA6U8kEqotwjxh2IC0yAS2SdOWuBoCrasQAvD_BwE&utm_medium=cpc&utm_source=gp_pla&IdSku=11615781&idLojista=16&s_kwcid=AL!427!3!97121726218!!!g!407312244858!&utm_campaign=Elpo_Shopping&ef_id=WjmZQwAAANodVQ2Z:20180913034551:s","nome_presente":"Air Fryer - R$ 299,90","descricao_presente":""}
+];
 
 
 
